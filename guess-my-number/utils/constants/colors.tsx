d5a43a0e7c70f2d79a0fb190b25bd38fcab874ce
Matheus Kieling <matheus.kieling@serendipe.org>
2025-08-29 10:28:50 -0300

    style: changing colors

---
+++ guess-my-number/utils/constants/colors.tsx
@@ -0,0 +1,10 @@
+﻿const Colors = {
+  primary400: '#72063c',
+  primary500: '#640233',
+  primary600: '#4e0329',
+  primary700: '#3b021f',
+  accent400: '#ddb52f',
+  accent500: '#d3af19',
+}
+
+export default Colors;
