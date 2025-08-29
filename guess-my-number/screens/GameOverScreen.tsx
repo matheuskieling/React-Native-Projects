1575a09bd9546b0bddb978778c11d1850c9a2bc7
Matheus Kieling <matheus.kieling@serendipe.org>
2025-08-29 10:27:54 -0300

    feat: game over screen

---
+++ guess-my-number/screens/GameOverScreen.tsx
@@ -0,0 +1,72 @@
+import { View, Text, StyleSheet, Image, ImageResizeMode } from 'react-native';
+import { useGameContext } from '../providers/GameProvider';
+import AppText from '../components/ui/AppText';
+import Title from '../components/ui/Title';
+import Colors from '../utils/constants/colors';
+import App from '../App';
+import PrimaryButton from '../components/ui/PrimaryButton';
+
+export default function GameOverScreen() {
+  const { pickedNumber, rounds, handleResetGame } = useGameContext();
+  return  (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Title>GAME OVER</Title>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image}  source={require('../assets/images/success.png')} />
+        </View>
+        <AppText style={styles.text}>
+          Your number is
+          <AppText style={styles.innerText} bold={true}> {pickedNumber}</AppText>
+          . Your phone took
+          <AppText style={styles.innerText} bold={true}> {rounds} </AppText>
+          rounds to guess it
+        </AppText>
+      </View>
+      <View style={styles.buttonContainer}>
+        <PrimaryButton onPress={handleResetGame}>Start new Game</PrimaryButton>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    marginVertical: 'auto',
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    overflow: 'hidden',
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 'auto',
+    marginVertical: 36,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary700,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  innerText: {
+    color: Colors.primary400,
+  },
+  buttonContainer: {
+    marginVertical: 'auto'
+  }
+});
